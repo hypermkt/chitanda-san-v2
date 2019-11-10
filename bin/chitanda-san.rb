0b7a3af86a4fc23fa48f908a3f8d37a3fe7d2da9
Makoto Chiba <hypermkt@gmail.com>
2019-11-10 23:15:27 +0900

    Split runner to bin file

---
+++ bin/chitanda-san.rb
@@ -0,0 +1,4 @@
+require 'dotenv/load'
+require_relative '../lib/chitanda_san'
+
+ChitandaSan.run
---
+++ lib/chitanda_san.rb
@@ -0,0 +1,17 @@
+require_relative 'chitanda-san/syoboi_calendar/client'
+require_relative 'chitanda-san/syoboi_calendar/parser'
+require_relative 'chitanda-san/notifier/slack'
+require_relative 'chitanda-san/message'
+
+module ChitandaSan
+  def self.run
+    client = ChitandaSan::SyoboiCalendar::Client.new
+    response = client.get
+    items = ChitandaSan::SyoboiCalendar::Parser.parse(response.body)
+
+    message = ChitandaSan::Message.create items
+
+    notifier = ChitandaSan::Notifier::Slack.new
+    notifier.post message
+  end
+end
