c45365c8026deaeb3308095eaca2591020dc150b
Makoto Chiba <hypermkt@gmail.com>
2020-01-21 22:21:34 +0900

    Remove unused variable

--- lib/chitanda-san/syoboi_calendar/repositories/rss2.rb
+++ lib/chitanda-san/syoboi_calendar/repositories/rss2.rb
@@ -4,12 +4,10 @@ module ChitandaSan
       class Rss2
         def get_items(start_date: , end_date: , days:)
           response = client.rss2(start_date: start_date, end_date: end_date, days: days)
-          body = response.body['rss']['channel']['item'].map do |item|
+          response.body['rss']['channel']['item'].map do |item|
             splitted_item = item['title'].split('##')
             ChitandaSan::SyoboiCalendar::Models::Rss2.new(splitted_item)
           end
-
-          body
         end
 
         private
