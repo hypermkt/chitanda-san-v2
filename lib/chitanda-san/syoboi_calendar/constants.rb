94065aac69480c483b501f6cb2cb66cbd3c30c94
Makoto Chiba <hypermkt@gmail.com>
2020-01-21 22:21:02 +0900

    Remove unused

--- lib/chitanda-san/syoboi_calendar/constants.rb
+++ lib/chitanda-san/syoboi_calendar/constants.rb
@@ -2,9 +2,6 @@ module ChitandaSan
   module SyoboiCalendar
     module Constants
       BASE_URL = 'http://cal.syoboi.jp'
-      DAYS = 1
-      START_HOUR = 19
-      END_HOUR = 3
     end
   end
 end
