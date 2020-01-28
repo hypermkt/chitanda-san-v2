6a3febc3803e6fdaea98cfeb8479e6e41e017089
Makoto Chiba <hypermkt@gmail.com>
2020-01-28 22:33:56 +0900

    チャネルグループ指定だと余計なチャンネルが入るので、チャンネル個別指定に変えた

--- lib/chitanda-san/syoboi_calendar/constants.rb
+++ lib/chitanda-san/syoboi_calendar/constants.rb
@@ -2,6 +2,20 @@ module ChitandaSan
   module SyoboiCalendar
     module Constants
       BASE_URL = 'http://cal.syoboi.jp'
+      CHANNELS = {
+        TBS: 5,
+        TOKYO_MX: 19,
+        TOKYO_MX2: 187,
+        tvk: 8,
+        TOCHIGI_TV: 100,
+        CHIBA_TV: 13,
+        TV_ASAHI: 6,
+        TV_TOKYO: 7,
+        TELETAMA: 14,
+        FUJITV: 3,
+        NTV: 4,
+        GTV: 72
+      }
     end
   end
 end
